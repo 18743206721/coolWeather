4c697185e625180209e4f945c906be41947fcf5d
liuyu <ll@hellokey.com>
2017-04-06 10:57:56 +0800

    First commit

---
+++ app/src/main/java/com/liuyu/weather/contus/Contus.java
@@ -0,0 +1,18 @@
+package com.liuyu.weather.contus;
+
+/**
+ * Created by Administrator on 2017/4/6.
+ */
+public class Contus {
+
+    //所有的省份,市县，后面加上id；
+    public static final String PRIVICE_CITY="http://guolin.tech/api/china";
+
+    //访问天气信息的接口
+    //个人认证key: 0831e56c8388489286c3e85cfa2ef375
+    //http://guolin.tech/api/weather?cityid=CN101190401&key=0831e56c8388489286c3e85cfa2ef375
+    public static final String WEATHER="http://guolin.tech/api/weather?";
+
+
+
+}
